daeae44f07e76889781b765fcdd8624e0204a542
kazuma516 <kizuma.9516@gmail.com>
2022-12-31 10:04:23 +0900

    itemモデル記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,34 @@
-class Item < ApplicationRecord
+class Item < ApplicationRecord 
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+  belongs_to :user
+  has_many   :favorite
+
+
+  belongs_to :category
+  belongs_to :where_to_buy
+  belongs_to :budgets
+  belongs_to :four_season
+
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :user
+    validates :title
+    validates :introduction
+    validates :buy_prefecture_id
+    validates :price_id
+    validates :image
+    validates :category_id
+    validates :season_id
+  end
+
+    #validates :price, numericality: { only_integer: true }
+
+  with_options numericality: { other_than: 0 } do
+    validates :buy_prefecture_id
+    validates :price_id
+    validates :category_id
+    validates :season_id
+  end
 end
